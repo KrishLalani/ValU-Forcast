abe8e0bde6b319ca88139ced6915c9894b9a1246
Jeel Patel <124356989+jeelpatel001@users.noreply.github.com>
2024-07-10 16:05:24 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,21 @@
+// const dotenv = require('dotenv');
+// const app = require('./app.js');
+
+// dotenv.config({
+//     path: './.env'
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
 const dotenv = require('dotenv');
-const app = require('./app.js');
+const app = require('./app'); // Ensure app.js is in the same directory
 
 dotenv.config({
-    path: './.env'
+    path: './.env' // Ensure the .env file is in the root directory
 });
 
 const PORT = process.env.PORT || 3000;
