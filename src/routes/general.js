11126197def606b577e019db07caffb4602b619e
Jeel Patel <124356989+jeelpatel001@users.noreply.github.com>
2024-07-12 20:07:15 +0530

    Update general.js

--- src/routes/general.js
+++ src/routes/general.js
@@ -3,35 +3,26 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const mysql = require('mysql');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const moment = require('moment');
-const vverifyToken = require('../middlewares/middleware.js');
-
-// Secret key for JWT (stored securely in environment variables)
-const JWT_SECRET = process.env.JWT_SECRET;
-
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-const { verifyToken, checkRole } = vverifyToken;
 
 // Database connection
 const connection = mysql.createConnection({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
-
 connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err.stack);
-    return;
-  }
-  console.log('Connected to MySQL as id', connection.threadId);
+    if (err) {
+        console.error('Error connecting to MySQL:', err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id', connection.threadId);
 });
 
 
@@ -40,17 +31,32 @@ router.get('/validate-user/:uid', (req, res) => {
   
     const query = 'SELECT * FROM user WHERE uid = ?';
     connection.query(query, [uid], (err, results) => {
-        if (err) {
-            res.status(500).json({ status: 'error', message: err.message });
-            return;
-        }
-      
-        if (results.length > 0) {
-            res.status(200).json({ message: 'user_exists', type: results[0].type });
-        } else {
-            res.status(200).json({ message: 'user_not_exists' });
-        }
+      if (err) {
+        res.status(500).json({ status: 'error', message: err.message });
+        return;
+      }
+  
+      if (results.length > 0) {
+        res.status(200).json({ message: 'user_exists',
+            "type" : results[0].type
+         });
+      } else {
+        res.status(200).json({ message: 'please_sign_up',
+         });
+      }
     });
+});
+router.get('/users', (req, res) => {
+  const query = 'SELECT * FROM user';
+  
+  connection.query(query, (err, results) => {
+      if (err) {
+          res.status(500).json({ status: 'error', message: err.message });
+          return;
+      }
+
+      res.status(200).json({ status: 'success', data: results });
+  });
 });
 
 module.exports = router;
