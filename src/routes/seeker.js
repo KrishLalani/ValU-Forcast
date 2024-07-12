cb2061f06cae6655d06c5ab76c1219d963069cfa
Jeel Patel <124356989+jeelpatel001@users.noreply.github.com>
2024-07-12 17:16:18 +0530

    Update seeker.js
    Database connection

--- src/routes/seeker.js
+++ src/routes/seeker.js
@@ -18,10 +18,12 @@ const { verifyToken, checkRole } = vverifyToken;
 
 // Database connection
 const connection = mysql.createConnection({
+    port:process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
     timezone: "Z",
     connectTimeout: 10000 // 10 seconds
 });
